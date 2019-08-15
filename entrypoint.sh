8d1e18e34353eba849282437a97e0a3527d31d51
Christian Stefanescu <christian.stefanescu@adac-camping.de>
2019-08-15 15:33:01 +0200

    Handle SIGTERM passthrough
    Based on https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,4 +45,25 @@ env_secrets_expand() {
 
 env_secrets_expand
 
-exec "$@"
+# set up SIGTERM handler
+pid=0
+
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+
+exec "$@" &
+
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
